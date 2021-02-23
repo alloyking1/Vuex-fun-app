b55c6b29be0328dc220bbec88bd4b2467cd27cc1
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-02-23 10:17:21 -0500

    Renamed Files and made first component

---
+++ src/components/Watson/WatsonComp1.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div>
+        Welcome to my components. For more info click <a href="https://www.youtube.com/watch?v=0YHx4tMk0Vs" target="_blank">HERE</a>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Watson/WatsonComp2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div />
+    <!--<audio controls autoplay>
+        <source src="./music.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio> -->
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+</style>
